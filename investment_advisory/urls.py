c12052f680edcc5d9884ba34918d60b0c0b56ff1
Rounak2001 <rounakpatel2211@gmail.com>
2025-12-11 16:54:04 +0530

    added 2b rec urls

--- investment_advisory/urls.py
+++ investment_advisory/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/', include('chat_api.urls')),
     path('api/gstr/', include('gstr1vs3b.urls')),
     path('api/bot/', include('bot.urls')),
+    path('api/', include('reconciliation.urls')),
 ]
