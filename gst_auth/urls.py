cdc092781960d87358d6dc5a537b1ac529875343
advait2711 <137433670+advait2711@users.noreply.github.com>
2026-01-02 16:14:31 +0530

    update  URL pattern
    Comment out the session status URL pattern.

--- gst_auth/urls.py
+++ gst_auth/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('generate-otp/', views.generate_otp, name='gst_auth_generate_otp'),
     path('verify-otp/', views.verify_otp, name='gst_auth_verify_otp'),
-    path('session-status/', views.session_status, name='gst_auth_session_status'),
+    # path('session-status/', views.session_status, name='gst_auth_session_status'),
 ]
