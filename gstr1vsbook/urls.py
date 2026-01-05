0e26cdddf4e8e0d8765b1b91e4084864c400f9ea
Advait <advaitsanoj@gmail.com>
2026-01-05 11:25:09 +0530

    update gstr1vsbook urls

--- gstr1vsbook/urls.py
+++ gstr1vsbook/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from .views import (
-    GSTR1GenerateOTPAPIView,
-    GSTR1VerifyOTPAPIView,
     GSTR1ReconciliationAPIView,
     GSTR1ExcelDownloadAPIView
 )
 
 urlpatterns = [
-    path('generate-otp/', GSTR1GenerateOTPAPIView.as_view(), name='gstr1-generate-otp'),
-    path('verify-otp/', GSTR1VerifyOTPAPIView.as_view(), name='gstr1-verify-otp'),
+    # Reconciliation endpoints (authentication via gst_auth session)
     path('reconcile/', GSTR1ReconciliationAPIView.as_view(), name='gstr1-reconciliation'),
     path('download-excel/', GSTR1ExcelDownloadAPIView.as_view(), name='gstr1-download-excel'),
 ]
