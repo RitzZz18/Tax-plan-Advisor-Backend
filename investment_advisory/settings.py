f0f70d8a0fcff9240a18164c358f6a53d1513f6d
Rounak2001 <rounakpatel2211@gmail.com>
2026-01-12 18:43:04 +0530

    static file for swagger

--- investment_advisory/settings.py
+++ investment_advisory/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     'django.middleware.common.CommonMiddleware',
@@ -207,5 +208,7 @@ USE_TZ = True
 # STATIC
 # -------------------------------------------------------
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
