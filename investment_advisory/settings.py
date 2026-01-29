544edd71667e8132154afa7e26d270ceaa730592
Advait <advaitsanoj@gmail.com>
2026-01-29 12:16:25 +0530

    update settings.py

--- investment_advisory/settings.py
+++ investment_advisory/settings.py
@@ -68,7 +68,8 @@ INSTALLED_APPS = [
     'gstr1toexcel',
     'reconciliation',
     'gstr1vsbook',
-    'calculator'
+    'calculator',
+    'tds_api',
 ]
 
 # -------------------------------------------------------
@@ -98,6 +99,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
